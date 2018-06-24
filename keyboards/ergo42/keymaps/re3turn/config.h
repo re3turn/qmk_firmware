12006651309bc441933ba5aefd14b3d5ed736ecd
re3turn <re3turn@gmail.com>
2018-06-24 10:44:52 +0900

    Update keymap

--- keyboards/ergo42/keymaps/re3turn/config.h
+++ keyboards/ergo42/keymaps/re3turn/config.h
@@ -34,4 +34,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define MASTER_RIGHT
 // #define EE_HANDS
 
+#define PREVENT_STUCK_MODIFIERS
+#define PERMISSIVE_HOLD
+#define TAPPING_TERM 100
+
 #endif
--- keyboards/ergo42/keymaps/re3turn/keymap.c
+++ keyboards/ergo42/keymaps/re3turn/keymap.c
@@ -12,66 +12,73 @@ extern keymap_config_t keymap_config;
 #define _______ KC_TRNS
 #define XXXXXXX KC_NO
 
+#define LTSY_SPC LT(SYMB, KC_SPC)
+#define LTME_ENT LT(META, KC_ENT)
+#define IME LALT(KC_GRV)
+#define ALTT_BSP ALT_T(KC_BSPC)
+#define CLTT_ESC CTL_T(KC_ESC)
+#define CLTT_BSL RCTL_T(KC_BSLS)
+#define SFTT_TAB SFT_T(KC_TAB)
+#define GUI_TAB LGUI(KC_TAB)
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* BASE
  * ,------------------------------------------------.   ,------------------------------------------------.
- * |   `  |   Q  |   W  |   E  |   R  |   T  |  [   |   |  ]   |   Y  |   U  |   I  |   O  |   P  |  @   |
- * |------+------+------+------+------+------+------|   |-------------+------+------+------+------+------|
- * |   =  |   A  |   S  |   D  |   F  |   G  |  (   |   |  )   |   H  |   J  |   K  |   L  |   ;  |  '   |
- * |------+------+------+------+------+------+------|   |------|------+------+------+------+------+------|
+ * |   `  |   Q  |   W  |   E  |   R  |   T  |  [   |   |  ]   |   Y  |   U  |   I  |   O  |   P  |  =   |
+ * |------+------+------+------+------+------+------|   |------+\-----+------+------+------+------+------|
+ * | Alt  |   A  |   S  |   D  |   F  |   G  |  (   |   |  )   |   H  |   J  |   K  |   L  |   ;  |  '   |
+ * |------+------+------+------+------+------+------|   |------+------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  |  {   |   |  }   |   N  |   M  |   ,  |   .  |   /  |  -   |
  * |------+------+------+------+------+------+------|   |------+------+------+------+------+------+------|
- * | Ctrl | GUI  | Alt  | IME  |ESC/  |Space/|Tab/  |   | BS/  |Enter/| Del  | GUI+ |=>META|=>SYMB|  \   |
- * |      |      |      |      |~SYMB |Ctrl  |Shift |   | Alt  |~META |      | Tab  |      |      |      |
+ * | Ctrl | GUI  | Alt  | IME  | Esc/ |Space/| Tab/ |   | BS/  |Enter/| Del  | GUI+ | Alt  | GUI  |  \   |
+ * |      |      |      |      | Ctrl |~SYMB | Shift|   | Alt  |~META |      | Tab  |      |      |      |
  * `------------------------------------------------'   `------------------------------------------------'
  */
 
 [BASE] = KEYMAP( \
-  KC_GRV,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LBRC, KC_RBRC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_AT, \
-  KC_EQL,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_LPRN, KC_RPRN, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,\
-  KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LCBR, KC_RCBR, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_MINS, \
-  KC_LCTRL, KC_LGUI, KC_LALT, LALT(KC_GRV), LT(SYMB, KC_ESC), RCTL_T(KC_SPC), SFT_T(KC_TAB), ALT_T(KC_BSPC), LT(META, KC_ENT), KC_DEL, LGUI(KC_TAB), TG(META), TG(SYMB), KC_BSLS \
+  KC_GRV,  KC_Q,    KC_W,    KC_E, KC_R,     KC_T,     KC_LBRC,  KC_RBRC,  KC_Y,     KC_U,   KC_I,    KC_O,    KC_P,    KC_EQL, \
+  KC_LALT, KC_A,    KC_S,    KC_D, KC_F,     KC_G,     KC_LPRN,  KC_RPRN,  KC_H,     KC_J,   KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
+  KC_LSFT, KC_Z,    KC_X,    KC_C, KC_V,     KC_B,     KC_LCBR,  KC_RCBR,  KC_N,     KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_MINS, \
+  KC_LCTL, KC_LGUI, KC_LALT, IME,  CLTT_ESC, LTSY_SPC, SFTT_TAB, ALTT_BSP, LTME_ENT, KC_DEL, GUI_TAB, KC_RALT, KC_RGUI, KC_BSLS \
 ),
 
 /* META
  * ,------------------------------------------------.   ,------------------------------------------------.
- * |   1  |   2  |   3  |   4  |   5  |   6  |  [   |   |  ]   |   7  |   8  |   9  |   0  |   -  |  =   |
+ * |   `  |   1  |   2  |   3  |   4  |   5  |  [   |   |  ]   |   6  |   7  |   8  |   9  |   0  |  -   |
  * |------+------+------+------+------+------+------|   |-------------+------+------+------+------+------|
- * |      |      | PgUp | Home | End  | PgDn |  (   |   |  )   | Left | Down |  Up  | Right|      |      |
+ * |      |PtrScr| PgUp | Home | End  | PgDn |  (   |   |  )   | Left | Down |  Up  | Right| F11  | F12  |
  * |------+------+------+------+------+------+------|   |------|------+------+------+------+------+------|
- * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  {   |   |  }   |  F7  |  F8  |  F9  | F10  | F11  | F12  |
+ * |      |  F1  |  F2  |  F3  |  F4  |  F5  |  {   |   |  }   |  F6  |  F7  |  F8  |  F9  | F10  |      |
  * |------+------+------+------+------+------+------|   |------+------+------+------+------+------+------|
- * | Ctrl | GUI  | Alt  | IME  |ESC/  |Space/|Tab/  |   | BS/  |Enter/| Del  | GUI+ |=>META|=>SYMB|      |
- * |      |      |      |      |~SYMB |Ctrl  |Shift |   | Alt  |~META |      | Tab  |      |      |      |
+ * |      |      |      |      |      |      |      |   |      |      |      |      |      |      |      |
  * `------------------------------------------------'   `------------------------------------------------'
  */
 
 [META] = KEYMAP( \
-  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    _______, _______, KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL, \
-  XXXXXXX, XXXXXXX, KC_PGUP, KC_HOME, KC_END,  KC_PGDN, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX, XXXXXXX, \
-  KC_F1  , KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   _______, _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, \
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, XXXXXXX \
+  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    _______, _______, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, \
+  _______, KC_PSCR, KC_PGUP, KC_HOME, KC_END,  KC_PGDN, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_F11,  KC_F12, \
+  _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______, _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______, \
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ \
 ),
 
 /* SYMB
  * ,------------------------------------------------.   ,------------------------------------------------.
- * |   !  |   @  |   #  |   $  |   %  |   ^  |  [   |   |  ]   |   &  |   *  |   (  |   )  |   _  |  +   |
+ * |   ~  |   !  |   @  |   #  |   $  |   %  |  [   |   |  ]   |   ^  |   &  |   *  |   (  |   )  |   _  |
  * |------+------+------+------+------+------+------|   |-------------+------+------+------+------+------|
- * |      |      |      |      |      |      |  (   |   |  )   |      |      |      |      |   {  |  }   |
+ * |      |      |      |      |      |      |  (   |   |  )   |      |      |      |   {  |   }  |   +  |
  * |------+------+------+------+------+------+------|   |------|------+------+------+------+------+------|
- * | Shift|      |      |      |      |      |  {   |   |  }   |      |      |   <  |   >  |   ?  |  _   |
+ * |      |      |      |      |      |      |  {   |   |  }   |      |      |      |   [  |   ]  |      |
  * |------+------+------+------+------+------+------|   |------+------+------+------+------+------+------|
- * | Ctrl | GUI  | Alt  | IME  |ESC/  |Space/|Tab/  |   | BS/  |Enter/| Del  | GUI+ |=>META|=>SYMB|  |   |
- * |      |      |      |      |~SYMB |Ctrl  |Shift |   | Alt  |~META |      | Tab  |      |      |      |
+ * |      |      |      |      |      |      |      |   |      |      |      |      |      |      |      |
  * `------------------------------------------------'   `------------------------------------------------'
  */
 
 [SYMB] = KEYMAP( \
-  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, _______, _______, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_UNDS, KC_PLUS, \
-  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_LCBR, KC_RCBR, \
-  KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______, XXXXXXX, XXXXXXX, KC_LT,   KC_GT,   KC_QUES, KC_UNDS, \
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_PIPE \
+  KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, _______, _______, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_UNDS, \
+  _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______, XXXXXXX, XXXXXXX, XXXXXXX, KC_LCBR, KC_RCBR, KC_PLUS, \
+  _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______, XXXXXXX, XXXXXXX, XXXXXXX, KC_LBRC, KC_RBRC, _______, \
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ \
 ),
 
 };
