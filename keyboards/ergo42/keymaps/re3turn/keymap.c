34629a7b7352a50d9ea2e4bc81ac598d9df01eec
re3turn <re3turn@gmail.com>
2018-06-28 20:39:24 +0900

    Update ergo42 keymap

--- keyboards/ergo42/keymaps/re3turn/keymap.c
+++ keyboards/ergo42/keymaps/re3turn/keymap.c
@@ -15,7 +15,6 @@ extern keymap_config_t keymap_config;
 #define LTSY_SPC LT(SYMB, KC_SPC)
 #define LTME_ENT LT(META, KC_ENT)
 #define IME LALT(KC_GRV)
-#define ALTT_BSP ALT_T(KC_BSPC)
 #define CLTT_ESC CTL_T(KC_ESC)
 #define CLTT_BSL RCTL_T(KC_BSLS)
 #define SFTT_TAB SFT_T(KC_TAB)
@@ -31,16 +30,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------|   |------+------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  |  {   |   |  }   |   N  |   M  |   ,  |   .  |   /  |  -   |
  * |------+------+------+------+------+------+------|   |------+------+------+------+------+------+------|
- * | Ctrl | GUI  | Alt  | IME  | Esc/ |Space/| Tab/ |   | BS/  |Enter/| Del  | GUI+ | Alt  | GUI  |  \   |
- * |      |      |      |      | Ctrl |~SYMB | Shift|   | Alt  |~META |      | Tab  |      |      |      |
+ * | Ctrl | GUI  | Alt  | IME  | Esc/ |Space/| Tab/ |   | BS   |Enter/| Del  | GUI+ | Alt  | GUI  |  \   |
+ * |      |      |      |      | Ctrl |~SYMB | Shift|   |      |~META |      | Tab  |      |      |      |
  * `------------------------------------------------'   `------------------------------------------------'
  */
 
-[BASE] = KEYMAP( \
+[BASE] = LAYOUT( \
   KC_GRV,  KC_Q,    KC_W,    KC_E, KC_R,     KC_T,     KC_LBRC,  KC_RBRC,  KC_Y,     KC_U,   KC_I,    KC_O,    KC_P,    KC_EQL, \
   KC_LALT, KC_A,    KC_S,    KC_D, KC_F,     KC_G,     KC_LPRN,  KC_RPRN,  KC_H,     KC_J,   KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   KC_LSFT, KC_Z,    KC_X,    KC_C, KC_V,     KC_B,     KC_LCBR,  KC_RCBR,  KC_N,     KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_MINS, \
-  KC_LCTL, KC_LGUI, KC_LALT, IME,  CLTT_ESC, LTSY_SPC, SFTT_TAB, ALTT_BSP, LTME_ENT, KC_DEL, GUI_TAB, KC_RALT, KC_RGUI, KC_BSLS \
+  KC_LCTL, KC_LGUI, KC_LALT, IME,  CLTT_ESC, LTSY_SPC, SFTT_TAB, KC_BSPC,  LTME_ENT, KC_DEL, GUI_TAB, KC_RALT, KC_RGUI, KC_BSLS \
 ),
 
 /* META
@@ -55,7 +54,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `------------------------------------------------'   `------------------------------------------------'
  */
 
-[META] = KEYMAP( \
+[META] = LAYOUT( \
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    _______, _______, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, \
   _______, KC_PSCR, KC_PGUP, KC_HOME, KC_END,  KC_PGDN, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_F11,  KC_F12, \
   _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______, _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______, \
@@ -74,7 +73,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `------------------------------------------------'   `------------------------------------------------'
  */
 
-[SYMB] = KEYMAP( \
+[SYMB] = LAYOUT( \
   KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, _______, _______, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_UNDS, \
   _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______, XXXXXXX, XXXXXXX, XXXXXXX, KC_LCBR, KC_RCBR, KC_PLUS, \
   _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______, XXXXXXX, XXXXXXX, XXXXXXX, KC_LBRC, KC_RBRC, _______, \
